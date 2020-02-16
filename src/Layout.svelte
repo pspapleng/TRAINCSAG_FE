315559b0aa021aa2bc79dd170c99eb3554b7ddc7
pspapleng <62070039@kmitl.ac.th>
2020-02-16 15:13:09 +0700

    commit

--- src/Layout.svelte
+++
@@ -1,8 +0,0 @@
-<main>
-    <slot></slot>
-</main>
-<style>
-    main{
-        height: 100vh;
-    }
-</style>
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const user = writable({
-    username:'',
-})
-
-export const postID = writable(null)
-export const pages = writable("Login"); // อยากให้ค่าเริ่มต้นเป็น 'Login'
